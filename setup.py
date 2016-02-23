ca0cac6ee74db1e65f4faebc35c4c624934fbba2
Aleksey <NeverWalkAloner@gmail.com>
2016-02-23 11:22:46 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version='1.0',
     packages=find_packages(),
     long_description='Virgil keys service SDK',
-    data_files=[('VirgilSDK/virgil_crypto', ['VirgilSDK/virgil_crypto/_virgil_crypto_python.pyd'])],
+    data_files=[('VirgilSDK/virgil_crypto', ['VirgilSDK/virgil_crypto/_virgil_crypto_python.pyd',
+    'VirgilSDK/virgil_crypto/_virgil_crypto_python_64.pyd'])],
 )
