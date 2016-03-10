c4f5a806cc8812e6fe2ed7fa1df9cae702e6fd30
Aleksey <NeverWalkAloner@gmail.com>
2016-03-10 10:26:11 +0500

    Update IP messaging example

--- Examples/IPMessaging/client.py
+++ Examples/IPMessaging/client.py
@@ -7,31 +7,57 @@ from socket import *
 from VirgilSDK import virgilhub, helper
 import VirgilSDK.virgil_crypto.cryptolib as cryptolib
 
-token = '%TOKEN%'
-ident_link = 'https://identity-stg.virgilsecurity.com/v1'
-virgil_card_link = 'https://keys-stg.virgilsecurity.com/v3'
-private_key_link = 'https://keyring-stg.virgilsecurity.com/v3'
-virgil_hub = virgilhub.VirgilHub(token, ident_link, virgil_card_link, private_key_link)
 
-message = 'hello world'
-prkey = '%SENDER_PRIVATE_KEY%'
-passw = '%PRIVATE_KEY_PASSWORD%'
-sign = cryptolib.CryptoWrapper.sign(message, prkey, passw)
-
-data = {'message': message,
-        'signature': helper.base64.b64encode(str(bytearray(sign)))}
-json_data = helper.Helper.json_dumps(data)
+# Initialization virgil application
+def virgil_init(token, ident_link, virgil_card_link, private_key_link):
+    virgil_hub = virgilhub.VirgilHub(token, ident_link, virgil_card_link, private_key_link)
+    return virgil_hub
 
 
-card = virgil_hub.virgilcard.search_card('%RECIPIENT%')[0]
-card_key = card['public_key']['public_key']
-card_id = card['id']
-encrypted = bytearray(cryptolib.CryptoWrapper.encrypt(json_data, card_id, card_key))
-host = 'localhost' 
-port = 80
-ip_channel = socket(AF_INET, SOCK_STREAM) 
-ip_channel.connect((host, port)) 
-ip_channel.send(encrypted) 
-data = ip_channel.recv(1024) 
-print('Client received: ' + str(data)) 
-ip_channel.close() 
+# Sign message using sender's private key 'prkey' and private key password 'passw'
+def sign_message(message, prkey, passw):
+    sign = cryptolib.CryptoWrapper.sign(message, prkey, passw)
+    return sign
+
+
+# Encrypt json serialized data using recipient public key downloaded from
+# virgil key service
+def encrypt_message(json_data, recipient):
+    card = virgil_hub.virgilcard.search_card(recipient)[0]
+    card_key = card['public_key']['public_key']
+    card_id = card['id']
+    encrypted = bytearray(cryptolib.CryptoWrapper.encrypt(json_data, card_id, card_key))
+    return encrypted
+
+
+# Sending signed and encrypted message to the server
+def send_to_server(message, server_identity, prkey, passw, sender):
+    sign = sign_message(message, prkey, passw)
+    data = {'message': message,
+            'signature': helper.base64.b64encode(str(bytearray(sign))),
+            'sender': sender}
+    json_data = helper.Helper.json_dumps(data)
+    encrypted = encrypt_message(json_data, server_identity)
+
+    host = 'localhost'
+    port = 80
+    ip_channel = socket(AF_INET, SOCK_STREAM)
+    ip_channel.connect((host, port))
+    ip_channel.send(encrypted)
+    data = ip_channel.recv(1024)
+    print('Client received: ' + str(data))
+    ip_channel.close()
+
+
+if __name__ == '__main__':
+    token = '%TOKEN%'
+    ident_link = 'https://identity-stg.virgilsecurity.com/v1'
+    virgil_card_link = 'https://keys-stg.virgilsecurity.com/v3'
+    private_key_link = 'https://keyring-stg.virgilsecurity.com/v3'
+    virgil_hub = virgil_init(token, ident_link, virgil_card_link, private_key_link)
+
+    message = 'hello world'
+    prkey = '%SENDER_PRIVATE_KEY%'
+    passw = '%SENDER_PRIVATE_KEY_PASSWORD%'
+
+    send_to_server(message, '%RECIPIENT_IDENTITY%', prkey, passw, '%SENDER_IDENTITY%')
--- Examples/IPMessaging/server.py
+++ Examples/IPMessaging/server.py
@@ -7,33 +7,58 @@ from socket import *
 from VirgilSDK import virgilhub, helper
 import VirgilSDK.virgil_crypto.cryptolib as cryptolib
 
-token = '%TOKEN%'
-ident_link = 'https://identity-stg.virgilsecurity.com/v1'
-virgil_card_link = 'https://keys-stg.virgilsecurity.com/v3'
-private_key_link = 'https://keyring-stg.virgilsecurity.com/v3'
-virgil_hub = virgilhub.VirgilHub(token, ident_link, virgil_card_link, private_key_link)
 
-prkey = '%RECIPIENT_PRIVATE_KEY%'
-passw = '%PRIVATE_KEY_PASSWORD%'
+# Initialization virgil application
+def virgil_init(token, ident_link, virgil_card_link, private_key_link):
+    virgil_hub = virgilhub.VirgilHub(token, ident_link, virgil_card_link, private_key_link)
+    return virgil_hub
 
-host = 'localhost' 
-port = 80 
-ip_channel = socket(AF_INET, SOCK_STREAM) 
-ip_channel.bind((host, port)) 
-ip_channel.listen(5) 
-while True: 
-    connection, address = ip_channel.accept()      
+
+# Decrypt received message 'encrypted' using private key 'prkey' and key password 'passw'
+def decrypt_message(encrypted, card_id, prkey, passw):
+    decrypted = cryptolib.CryptoWrapper.decrypt(encrypted, card_id, prkey, passw)
+    json_data = helper.Helper.json_loads(str(bytearray(decrypted)))
+    return json_data
+
+
+# Verify signature in json serialized data 'json_data' using sender identity 'sender'
+def verify_signature(json_data, sender):
+    card = virgil_hub.virgilcard.search_app(sender)[0]
+    card_key = card['public_key']['public_key']
+    is_signed = cryptolib.CryptoWrapper.verify(json_data['message'], json_data['signature'], card_key)
+    if not is_signed:
+        raise ValueError('Signature is invalid!')
+
+
+# run server to receive messages coming from clients
+# prkey - private key using for decryption
+# card_id - server's virgil card id
+def run_server(prkey, passw, card_id):
+    host = 'localhost'
+    port = 80
+    ip_channel = socket(AF_INET, SOCK_STREAM)
+    ip_channel.bind((host, port))
+    ip_channel.listen(5)
     while True:
-        encrypted = connection.recv(1024)
-        if not encrypted: break
-        card = virgil_hub.virgilcard.search_card('%SENDER%')[0]
-        card_key = card['public_key']['public_key']
-        card_id = card['id']
-        decrypted = cryptolib.CryptoWrapper.decrypt(encrypted, card_id, prkey, passw)
-        json_data = helper.Helper.json_loads(str(bytearray(decrypted)))
-        is_signed = cryptolib.CryptoWrapper.verify(json_data['message'], json_data['signature'], card_key)
-        if not is_signed:
-            raise ValueError('Signature is invalid!')
-        print(json_data['message'])
-        connection.send(b'Server received=>' + json_data['message'])
-    connection.close() 
+        connection, address = ip_channel.accept()
+        while True:
+            encrypted = connection.recv(1024)
+            print(encrypted)
+            if not encrypted: break
+            json_data = decrypt_message(encrypted, card_id, prkey, passw)
+            verify_signature(json_data, json_data['sender'])
+            print('decrypted:' + json_data['message'])
+            connection.send(b'Server received=>' + json_data['message'])
+        connection.close()
+
+
+if __name__ == '__main__':
+    token = '%TOKEN%'
+    ident_link = 'https://identity-stg.virgilsecurity.com/v1'
+    virgil_card_link = 'https://keys-stg.virgilsecurity.com/v3'
+    private_key_link = 'https://keyring-stg.virgilsecurity.com/v3'
+    virgil_hub = virgil_init(token, ident_link, virgil_card_link, private_key_link)
+
+    prkey = '%RECIPIENT_PRIVATE_KEY%'
+    passw = '%RECIPIENT_PRIVATE_KEY_PASSWORD%'
+    run_server(prkey, passw, '%RECIPIENT_CARD_ID%')
