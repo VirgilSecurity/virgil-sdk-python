055f618495fe03765f2ff4e547c91f3e12eaef20
Aleksey <NeverWalkAloner@gmail.com>
2016-04-19 22:09:48 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,17 @@ setup(
     long_description='Virgil keys service SDK',
     package_data={
         "VirgilSDK.virgil_crypto": [
-            "_virgil_crypto_python.pyd",
-            "_virgil_crypto_python_64.pyd",
-            "_virgil_crypto_python.so",
+            "virgil-crypto-1.4.0-python-2.7-darwin-14.5-universal/_virgil_crypto_python.so",
+            "virgil-crypto-1.4.0-python-2.7-linux-x86_64/_virgil_crypto_python.so",
+            "virgil-crypto-1.4.0-python-2.7-windows-6.3-x64/_virgil_crypto_python.pyd",
+            "virgil-crypto-1.4.0-python-2.7-windows-6.3-x86/_virgil_crypto_python.pyd",
+            "virgil-crypto-1.4.0-python-3.4-darwin-14.5-universal/_virgil_crypto_python.so",
+            "virgil-crypto-1.4.0-python-3.4-linux-x86_64/_virgil_crypto_python.so",
+            "virgil-crypto-1.4.0-python-3.4-windows-6.3-x64/_virgil_crypto_python.pyd",
+            "virgil-crypto-1.4.0-python-3.4-windows-6.3-x86/_virgil_crypto_python.pyd",
+            "virgil-crypto-1.4.0-python-3.5-darwin-14.5-universal/_virgil_crypto_python.so",
+            "virgil-crypto-1.4.0-python-3.5-windows-6.3-x64/_virgil_crypto_python.pyd",
+            "virgil-crypto-1.4.0-python-3.5-windows-6.3-x86/_virgil_crypto_python.pyd", 
         ],
     },
 )
