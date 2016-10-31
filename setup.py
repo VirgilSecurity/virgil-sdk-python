794ace368acd70a3f82ea2fcdd6f51dca09aadd7
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2016-10-31 23:01:22 +0200

    Temporary removed examples

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="virgil-sdk",
-    version="1.0",
+    version="4.0",
     packages=find_packages(),
     install_requires=[
         'virgil-crypto',
