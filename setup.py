c4a5c6ec5def8349c3ec3de6c41328160d0797e1
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2016-11-01 12:17:29 +0200

    Added package metadata

--- setup.py
+++ setup.py
@@ -2,10 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="virgil-sdk",
-    version="4.0",
+    version="4.0.0a",
     packages=find_packages(),
     install_requires=[
         'virgil-crypto',
     ],
+    author="Virgil Security",
+    url="https://virgilsecurity.com/",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Security :: Cryptography",
+        ],
+    license="BSD",
     long_description="Virgil keys service SDK",
 )
