b02f097229a244379f6efae6fe6c2e3e7de3f435
Imelstorm <const_ant_in@virgilsecurity.com>
2017-03-09 13:44:03 +0200

    [SDK-171] Create class Credentials

---
+++ virgil_sdk/api/credentials.py
@@ -0,0 +1,57 @@
+# Copyright (C) 2016 Virgil Security Inc.
+#
+# Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     (1) Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#     (2) Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#
+#     (3) Neither the name of the copyright holder nor the names of its
+#     contributors may be used to endorse or promote products derived from
+#     this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+class Credentials(object):
+
+    """Provides credentials for application authentication using app_id and app_key
+    retrieved from development deshboard."""
+    def __init__(
+        self,
+        app_id,  # type: str
+        app_key,  # type: bytes
+        app_key_password=None  # type: Optional[str]
+    ):
+        # type: (...) -> None
+        self.app_id = app_id
+        self.app_key = app_key
+        self.app_key_password = app_key_password
+
+    def get_app_key(self, crypto):
+        # type: (Crypto) -> PrivateKey
+        """Gets the application PrivateKey used to authenticate Publish/Revoke Card requests."""
+        if self.app_key_password:
+            return crypto.import_private_key(self.app_key, self.app_key_password)
+        else:
+            return crypto.import_private_key(self.app_key)
