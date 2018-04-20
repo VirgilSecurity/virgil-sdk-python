1672f12fd3a835e17c4b9f58ef37d40f765eff58
Imelstorm <const_ant_in@virgilsecurity.com>
2018-04-20 15:24:36 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="virgil-sdk",
-    version="4.2.2",
+    version="4.2.3",
     packages=find_packages(),
     install_requires=[
         'virgil-crypto',
