f2f1c8f4e88e0c0e6d3c0bd953c76f15aaba64d3
Imelstorm <const_ant_in@virgilsecurity.com>
2018-11-16 13:28:14 +0200

    Deprecate support of python3.3

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
