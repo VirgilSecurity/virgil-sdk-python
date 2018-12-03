5e32aa176c440861ae2e1f9943b656c0cbf5dcb6
Imelstorm <imelstorm.cm@gmail.com>
2018-12-03 18:19:56 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="virgil-sdk",
-    version="5.0.1",
+    version="5.1.0",
     packages=find_packages(),
     package_data={"virgil_sdk": [
         "tests/*",
